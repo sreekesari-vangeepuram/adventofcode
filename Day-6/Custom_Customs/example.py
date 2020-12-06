e34e5d422c9d015b3a2e8bdf80a725e16242405d
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2020-12-06 12:07:06 +0530

    Day-6, COMPLETE!

---
+++ Day-6/Custom_Customs/example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+input_data = open("sample.txt").read().split("\n\n")
+
+groups = [set(x.strip().split("\n")) for x in input_data]
+
+filtered_responses = list() # list of sets
+tmp_set = set() # temporary set as buffer-space for each group
+
+for group in groups: # set
+	for person in group: # string
+		for answer in person: # letter
+			tmp_set.add(answer)
+	filtered_responses.append(tmp_set)
+	tmp_set = set() # killing the buffer
+
+counter = 0
+for grp_resps in filtered_responses:
+	counter += len(grp_resps)
+print(f"The final sum of the counts from the the input-set is: {counter}.")
