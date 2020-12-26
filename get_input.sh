1ae05455839374c6d82e7339406a80b3e0f513d9
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2020-12-26 19:54:40 +0530

    Automate input fetching

--- get_input.sh
+++ get_input.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-YEAR=2020
-curl --cookie session=$(cat $HOME/adventofcode.com/.session-cookie) "https://adventofcode.com/$YEAR/day/$1/input"
+YEAR=$1
+curl --cookie session=$(cat $HOME/adventofcode.com/.session-cookie) "https://adventofcode.com/$YEAR/day/$2/input"
