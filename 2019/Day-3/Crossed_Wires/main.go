e00f3175b5244ce1a0464a1b6fba441c632f1888
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2021-01-02 15:51:02 +0530

    AoC-2019 Day-3, COMPLETE!

--- 2019/Day-3/Crossed_Wires/main.go
+++ 2019/Day-3/Crossed_Wires/main.go
@@ -2,42 +2,42 @@ package main
 
 import (
     "os"
-    "fmt"
-    "io/ioutil"
     "log"
+    "io/ioutil"
+    "fmt"
     "strings"
     "strconv"
 )
 
-var direction map[string]Point
-
 type Instruction struct {
-    direction string
-    magnitude int
+    dir string
+    mag int
+}
+
+func parseInstructions(instructions []string) []Instruction {
+    buff_array := make([]Instruction, len(instructions))
+    for i, ins := range instructions {
+        mag, _ := strconv.Atoi(string(ins[1:]))
+        buff_array[i] = Instruction{string(ins[0]), mag}
+    }
+    return buff_array
 }
 
 type Point struct {
     x, y int
 }
 
+var direction map[string]Point
 func (p *Point) update(ins Instruction) {
-    point := direction[ins.direction]
+    point := direction[ins.dir]
     if point.x == 0 {
-                p.y += point.y * ins.magnitude
+                p.y += point.y * ins.mag
     } else {
-                p.x += point.x * ins.magnitude
+                p.x += point.x * ins.mag
     }
 }
 
 func main() {
-
-     direction = map[string]Point {
-        "R": Point{+1, 0},
-        "L": Point{-1, 0},
-        "U": Point{0, +1},
-        "D": Point{0, -1},
-    }
-
     if len(os.Args) < 2 {
         log.Fatal("** Usage: ./main <filename>")
     }
@@ -47,121 +47,137 @@ func main() {
         log.Fatal(err)
     }
 
-    buff_array := strings.Split(strings.Trim(string(data), "\n "), "\n")
-    wires := make([][]Instruction, len(buff_array))
-    for number, wire := range buff_array {
-        for _, ins := range strings.Split(wire, ",") {
-            num, err := strconv.Atoi(ins[1:])
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            wires[number] = append(wires[number], Instruction{string(ins[0]), num})
-        }
-        buff_array[number] = ""
+    var buff_array [][]string
+    for _, line := range strings.Split(strings.Trim(string(data), "\n "), "\n") {
+        buff_array = append(buff_array, strings.Split(line, ","))
     }
-    part1, part2 := solve(wires)
+
+    // Parse Instructions
+    wire1 := parseInstructions(buff_array[0])
+    wire2 := parseInstructions(buff_array[1])
+
+    c1 := make(chan Point, 1)
+    c2 := make(chan Point, 1)
+
+    direction = map[string]Point {
+        "R": Point{+1, 0},
+        "L": Point{-1, 0},
+        "U": Point{0, +1},
+        "D": Point{0, -1},
+    }
+
+    // Fetching tracks with the 
+    // obtained instructions `wire1`, `wire2`
+    var wire1Track, wire2Track []Point
+
+    go fetchTrack(wire1, c1)
+    for point1 := range c1 {
+        wire1Track = append(wire1Track, point1)
+    }
+
+    go fetchTrack(wire2, c2)
+    for point2 := range c2 {
+        wire2Track = append(wire2Track, point2)
+    }
+
+    var intersectionPoints []Point
+    /* Ignoring the 1st intersecting 
+     *  point at Point{0, 0}
+     */
+    for _, p1 := range wire1Track[1:] {
+        for _, p2 := range wire2Track[1:] {
+            if p1 == p2 {
+                intersectionPoints = append(intersectionPoints, p1)
+            }
+        }
+    }
+
+    var manhattanDistance, bfn1, bfn2 int
+    var countOfSteps []int
+
+    // PART - 1
+    part1 := int(^uint(0) >> 1)
+    for index, point := range intersectionPoints {
+        manhattanDistance = Abs(point.x) + Abs(point.y)
+        if index == 0 {
+            part1 = manhattanDistance
+        }
+
+        if manhattanDistance < part1 {
+            part1 = manhattanDistance
+        }
+    }
+
+    for _, point := range intersectionPoints {
+        bfn1, bfn2 = 0, 0
+        for i1, p1 := range wire1Track {
+            if p1 == point {
+                bfn1 = i1
+            }
+        }
+
+        for i2, p2 := range wire2Track {
+            if p2 == point {
+                bfn2 = i2
+            }
+        }
+
+        countOfSteps = append(countOfSteps, bfn1 + bfn2)
+    }
+
+    // PART - 2
+    part2 := countOfSteps[0]
+    for _, stepCount := range countOfSteps {
+        if stepCount < part2 {
+            part2 = stepCount
+        }
+    }
+
     fmt.Printf("Manhattan Distance: %d\n", part1)
     fmt.Printf("Fewest combined steps the wires must take to reach an intersection: %d\n", part2)
 }
 
-func intersectionsIn(locus *[][]Point) []Point {
-    buff_array := make([]Point, 0)
-    for _, point1 := range (*locus)[0] {
-        for _, point2 := range (*locus)[1] {
-            if (point1 == point2) && (point2 != Point{0, 0}) {
-                buff_array = append(buff_array, point1)
-            }
+func fetchTrack(instructions []Instruction, locus chan Point) {
+    currentLocation  := Point{0, 0}
+    previousLocation := Point{0, 0}
+    for i, instruction := range instructions {
+        if i < len(instructions)  {
+            previousLocation = currentLocation
         }
-    }
-    return buff_array
-}
 
-func Abs(n int) int {
-    if n < 0 {
-        return -n
-    }
-    return n
-}
+        currentLocation.update(instruction)
 
-func solve(wires [][]Instruction) (int, int) {
-    locus := make([][]Point, 2)
-
-    var currentLocation, previousLocation Point
-    for number, wire := range wires {
-        currentLocation  = Point{0, 0}
-        previousLocation = Point{0, 0}
-        for i, ins := range wire {
-            if i < len(wire)  {
-                previousLocation = currentLocation
-            }
-            currentLocation.update(ins)
-
-            if currentLocation.x - previousLocation.x == 0 {
-                if currentLocation.y < previousLocation.y {
-                    for y := previousLocation.y; y > currentLocation.y; y-- {
-                        locus[number] = append(locus[number], Point{currentLocation.x, y})
-                    }
-                } else {
-                    for y := previousLocation.y; y < currentLocation.y; y++ {
-                        locus[number] = append(locus[number], Point{currentLocation.x, y})
-                    }
+        if currentLocation.x - previousLocation.x == 0 {
+            if currentLocation.y < previousLocation.y {
+                for y := previousLocation.y; y > currentLocation.y; y-- {
+                    locus <- Point{currentLocation.x, y}
                 }
             } else {
-                if currentLocation.x < previousLocation.x {
-                    for x := previousLocation.x; x > currentLocation.x; x-- {
-                        locus[number] = append(locus[number], Point{x, currentLocation.y})
-                    }
-                } else {
-                    for x := previousLocation.x; x < currentLocation.x; x++ {
-                        locus[number] = append(locus[number], Point{x, currentLocation.y})
-                    }
+                for y := previousLocation.y; y < currentLocation.y; y++ {
+                    locus <- Point{currentLocation.x, y}
                 }
-
             }
+        } else {
+            if currentLocation.x < previousLocation.x {
+                for x := previousLocation.x; x > currentLocation.x; x-- {
+                    locus <- Point{x, currentLocation.y}
+                }
+            } else {
+                for x := previousLocation.x; x < currentLocation.x; x++ {
+                    locus <- Point{x, currentLocation.y}
+                }
+            }
+
         }
     }
-
-
-        var manhattanDistance, min1 int
-        intersectionPoints := intersectionsIn(&locus)
-        for index, point := range intersectionPoints {
-            manhattanDistance = Abs(point.x) + Abs(point.y)
-            if index == 0 {
-                min1 = manhattanDistance
-            }
-
-            if manhattanDistance < min1 {
-                min1 = manhattanDistance
-            }
-        }
-
-        countOfSteps := make([]int, 0)
-        var bfn1, bfn2 int
-        for _, point := range intersectionPoints {
-            bfn1, bfn2 = 0, 0
-            for i1, p1 := range locus[0] {
-                if p1 == point {
-                    bfn1 = i1
-                }
-            }
-
-            for i2, p2 := range locus[1] {
-                if p2 == point {
-                    bfn2 = i2
-                }
-            }
-
-            countOfSteps = append(countOfSteps, bfn1 + bfn2)
-        }
-
-        var min2 int = countOfSteps[0]
-        for _, stepCount := range countOfSteps {
-            if stepCount < min2 {
-                min2 = stepCount
-            }
-        }
-
-    return min1, min2
+    close(locus)
 
 }
+
+func Abs(number int) int {
+    if number < 0 {
+        return -number
+    }
+    return number
+}
+
